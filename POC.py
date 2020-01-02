c15b326b6cfa036a9dd280bb52ce61e001d8e4c7
Bouke Versteegh <info@boukeversteegh.nl>
2020-01-02 21:01:37 +0100

    Modules are using kafka as a message backend

--- POC.py
+++ POC.py
@@ -1,29 +1,72 @@
 import datetime
-from collections import namedtuple
+import json
+import logging
+import threading
+from dataclasses import dataclass
+
+from kafka import KafkaProducer, KafkaConsumer
 
 
 class Core:
-  def __init__(self):
+  def __init__(self, kafka_producer: KafkaProducer):
     self.modules = []
+    self.kafka_producer = kafka_producer
 
   def add_module(self, module):
+    logging.info("Registering: %s" % type(module).__name__)
     self.modules.append(module)
 
   def boot(self):
+    threads = []
     for module in self.modules:
-      module.boot(self)
+      logging.info("Booting module: %s" % type(module).__name__)
+
+      def module_runner():
+        module.boot(self)
+
+        module_consumer = KafkaConsumer(
+          TextInput.type,
+          bootstrap_servers='127.0.0.1:9092',  # todo extract kafka config
+          auto_offset_reset='earliest',
+          value_deserializer=json.loads
+        )
+
+        for message in module_consumer:
+          module.handle(message)
+
+      module_thread = threading.Thread(target=module_runner, daemon=True)
+      module_thread.start()
+      threads.append(module_thread)
+
+    logging.info("Booted all modules")
+
+    for thread in threads:
+      thread.join()
+
+    logging.info("Shutting down")
 
   def publish(self, message):
-    for module in self.modules:
-      module.handle(message)
+    self.kafka_producer.send(message.type, message.__dict__)
 
 
-TextInput = namedtuple('TextInput', ['text'])
+@dataclass
+class TextInput:
+  type = 'be.anagon.ai.poc.text.input'
+  text: str
 
 
-class TextInputModule:
+class BaseModule:
+  def boot(self, core: Core):
+    self.core = core
+
+  def handle(self, message):
+    pass
+
+
+class TextInputModule(BaseModule):
 
   def boot(self, core: Core):
+    super().boot(core)
     while True:
       text = input("Input: ")
       core.publish(TextInput(text=text))
@@ -32,29 +75,34 @@ class TextInputModule:
     pass
 
 
-class TextInputDisplayModule:
-  def boot(self, core: Core):
-    pass
-
+class TextInputDisplayModule(BaseModule):
   def handle(self, message):
-    print("You wrote: %s" % message.text)
-
-
-class BaseModule:
-  def boot(self, core: Core):
-    self.core = core
+    logging.info("You wrote: %s" % message.text)
 
 
 class TimeBot(BaseModule):
   def handle(self, message: TextInput):
     if message.text.find('time') > -1:
-      print('The time is: %s' % datetime.datetime.now().strftime('%H:%M:%S'))
+      print('The time is: %s' % datetime.datetime.now().strftime('%H:%M:%S'), flush=True)
+
+
+class HelloWorld(BaseModule):
+  def boot(self, core: Core):
+    super().boot(core)
+    print("Modular AI started", flush=True)
 
 
 if __name__ == '__main__':
-  ai = Core()
+  logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
+
+  producer = KafkaProducer(
+    bootstrap_servers=['127.0.0.1:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+  )
+  ai = Core(producer)
   ai.add_module(TextInputModule())
   ai.add_module(TextInputDisplayModule())
   ai.add_module(TimeBot())
+  ai.add_module(HelloWorld())
 
   ai.boot()
