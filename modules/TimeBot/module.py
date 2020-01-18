23322a5a9bdbb91d99f44afa2de533b9a6078dd8
Bouke Versteegh <info@boukeversteegh.nl>
2020-01-18 21:58:36 +0100

    Announce time every hour

--- modules/TimeBot/module.py
+++ modules/TimeBot/module.py
@@ -9,19 +9,20 @@ class TimeBot(BaseModule):
 
     def boot(self) -> None:
         self.subscribe(handler=self.handle, types=TextInput)
-        self.add_task(self.announce_time_every_minute())
+        self.add_task(self.announce_time_every_hour())
 
     def handle(self, event: TextInput) -> None:
         if event.text.find('time') > -1:
             self.announce_time()
 
-    async def announce_time_every_minute(self) -> None:
+    async def announce_time_every_hour(self) -> None:
         while True:
             time = datetime.datetime.now()
-            if time.second == 0:
+            if time.minute == 0 and time.second == 0:
                 self.announce_time()
 
-            await asyncio.sleep(1)
+            # sleep till the next minute
+            await asyncio.sleep(60 - time.second)
 
     def announce_time(self) -> None:
         self.publish(TextOutput(text='The time is: %s' % datetime.datetime.now().strftime('%H:%M:%S')))
